c3e3a5b205edd2a402fe64f5928503e1ca3c5413
Marek Kowalski <kowalski0123@gmail.com>
2013-07-24 14:56:36 +0200

    Integrate in sammy and mustache framework, define graph section with simple jump-to-id form.

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
   , path = require('path')
   ;
 
-ddoc = 
-  { _id:'_design/app'
-  , rewrites : 
+ddoc =
+  { _id:'_design/browser'
+  , rewrites :
     [ {from:"/", to:'index.html'}
     , {from:"/api", to:'../../'}
     , {from:"/api/*", to:'../../*'}
@@ -15,10 +15,10 @@ ddoc =
 
 ddoc.views = {};
 
-ddoc.validate_doc_update = function (newDoc, oldDoc, userCtx) {   
+ddoc.validate_doc_update = function (newDoc, oldDoc, userCtx) {
   if (newDoc._deleted === true && userCtx.roles.indexOf('_admin') === -1) {
     throw "Only admin can delete documents on this database.";
-  } 
+  }
 }
 
 couchapp.loadAttachments(ddoc, path.join(__dirname, 'attachments'));
--- attachments/site.js
+++ attachments/site.js
@@ -15,7 +15,7 @@ var request = function (options, callback) {
   $.ajax(options)
 }
 
-$.expr[":"].exactly = function(obj, index, meta, stack){ 
+$.expr[":"].exactly = function(obj, index, meta, stack){
   return ($(obj).text() == meta[3])
 }
 
@@ -24,8 +24,8 @@ var param = function( a ) {
   // This is important when converting datastructures to querystrings to send to CouchDB.
 	var s = [];
 	if ( jQuery.isArray(a) || a.jquery ) {
-		jQuery.each( a, function() { add( this.name, this.value ); });		
-	} else { 
+		jQuery.each( a, function() { add( this.name, this.value ); });
+	} else {
 	  for ( var prefix in a ) { buildParams( prefix, a[prefix] ); }
 	}
   return s.join("&");
@@ -34,9 +34,9 @@ var param = function( a ) {
 			jQuery.each( obj, function( i, v ) {
 				if (  /\[\]$/.test( prefix ) ) { add( prefix, v );
 				} else { buildParams( prefix + "[" + ( typeof v === "object" || jQuery.isArray(v) ? i : "") +"]", v )}
-			});				
+			});
 		} else if (  obj != null && typeof obj === "object" ) {
-			jQuery.each( obj, function( k, v ) { buildParams( prefix + "[" + k + "]", v ); });				
+			jQuery.each( obj, function( k, v ) { buildParams( prefix + "[" + k + "]", v ); });
 		} else { add( prefix, obj ); }
 	}
 	function add( key, value ) {
@@ -45,16 +45,36 @@ var param = function( a ) {
 	}
 }
 
+function render(template, target, data) {
+    if ( !data ) var data = {};
+    var html = $.mustache($("#" + template + "-template").text(), data);
+    $( "#" + target ).html(html);
+}
+
+
 var app = {};
 app.index = function () {
-  
+    render('welcome', 'main-container');
 };
 
-$(function () { 
+app.graph = function() {
+    render('graph', 'main-container', {docID: this.params['docID']});
+    return false;
+};
+
+app.jumpToHandler = function() {
+    this.log('here', this.params);
+    this.redirect('#', 'graph', this.params['docID']);
+};
+
+$(function () {
   app.s = $.sammy(function () {
     // Index of all databases
     this.get('', app.index);
     this.get("#/", app.index);
+    this.get("#graph", app.graph);
+    this.post("#graph", app.jumpToHandler);
+    this.get("#graph/:docID", app.graph);
   })
   app.s.run();
 });
